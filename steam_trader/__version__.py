01d6679f105300253e4da4442f3517702d09d5fd
Lemon4ksan <senya20151718@gmail.com>
2024-08-15 12:24:49 +0300

    Изменение версии

--- steam_trader/__version__.py
+++ steam_trader/__version__.py
@@ -1,3 +1,3 @@
-__version__ = '1.0.0'
+__version__ = '0.0.2'
 __license__ = 'BSD 3-Clause License'
-__copyright__ = 'Copyright (c) 2024, Lemon4ksan (aka Bananchiki) <https://github.com/Lemon4ksan>'
+__copyright__ = 'Copyright (c) 2024-present, Lemon4ksan (aka Bananchiki) <https://github.com/Lemon4ksan>'
