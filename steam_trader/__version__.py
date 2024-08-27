4ed71f084ba3275ec1a16656e6ecd02d0d2da532
Lemon4ksan <senya20151718@gmail.com>
2024-08-27 14:54:14 +0300

    Переход на 0.2.25

--- steam_trader/__version__.py
+++ steam_trader/__version__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.2'
+__version__ = '0.2.25'
 __license__ = 'BSD 3-Clause License'
 __copyright__ = 'Copyright (c) 2024-present, Lemon4ksan (aka Bananchiki) <https://github.com/Lemon4ksan>'
