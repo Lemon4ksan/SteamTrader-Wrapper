99249cd87dfff3ed58ac7487a3e3ac54301f9f40
Lemon4ksan <senya20151718@gmail.com>
2024-09-30 10:56:45 +0300

    Переход на 0.4.0

--- steam_trader/api/__version__.py
+++ steam_trader/api/__version__.py
@@ -1,3 +1,3 @@
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 __license__ = 'BSD 3-Clause License'
 __copyright__ = 'Copyright (c) 2024-present, Lemon4ksan (aka Bananchiki) <https://github.com/Lemon4ksan>'
